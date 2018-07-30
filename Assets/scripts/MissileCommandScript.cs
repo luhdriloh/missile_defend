d637c7d667cd23d8b40c35d7705ded0522a0d02f
luhdriloh <007.sam.moore@gmail.com>
2018-07-29 21:44:42 -0700

    Initial commit, added background and test code

---
+++ Assets/scripts/MissileCommandScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileCommandScript : MonoBehaviour
+{
+    public GameObject missile;
+    private Camera cam;
+
+    // Use this for initialization
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 position = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log("mouse position is: (" + position.x + ", " + position.y + ")");
+            GameObject x = Instantiate(missile, position, Quaternion.identity);
+
+            Quaternion target = Quaternion.Euler(0, 0, 45f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 1);
+
+            Debug.Log("position: " + x.ToString());
+
+
+        }
+    }
+}
---
+++ Assets/scripts/missiles/MissileMovementScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileMovementScript : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("missile movement");
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.AddForce(new Vector2(0, 10f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
